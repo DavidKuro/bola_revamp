c95aef8a46575196f1d71da9fef4117eb0b0ef36
Arthurobo <arthuron57@gmail.com>
2023-09-09 08:59:15 +0100

    New database

--- bolabule/settings.py
+++ bolabule/settings.py
@@ -87,27 +87,27 @@ WSGI_APPLICATION = 'bolabule.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# # Production Database
-# DB_NAME = "blog_db"
-# DB_USER = "blog"
-# DB_PASSWORD = "password"
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': DB_NAME,
-#         'USER': DB_USER,
-#         'PASSWORD': DB_PASSWORD,
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# Production Database
+DB_NAME = "den1ne1f0kpka2"
+DB_USER = "ypmvhypfhymjdp"
+DB_PASSWORD = "9b01a85c5ff498a625a7be802793770625737009ef598c9d1ca7bc02cf3809af"
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
